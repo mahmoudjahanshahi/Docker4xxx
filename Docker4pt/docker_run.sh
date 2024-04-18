4454a2acf5806f956fb3380a73c51029ba716f3a
mahmoudjahanshahi <mahmoud.jahanshahi@gmail.com>
2024-04-18 19:11:25 -0400

    installed required packages

--- Docker4pt/docker_run.sh
+++ Docker4pt/docker_run.sh
@@ -4,8 +4,8 @@ run="run"
 bash="bash"
 remove="rm"
 
-name="pt-nb"
-image="pt"
+name="pt-mahmoud"
+image="pt:latest"
 
 #-u $(id -u ${USER}):$(id -g ${USER}) \
 
@@ -13,12 +13,15 @@ if [ "$1" = "$run" ]; then
   docker run --gpus all -it --ipc=host \
 	-p 8889:8888 \
 	-p 6007:6006 \
-	-v /home/zyang37/:/home/zyang37/ \
-	-v /da2_data/shared:/da2_data/shared \
-	-v /home/mousavi:/home/mousavi \
-	-v /da1_data/shared:/da1_data/shared \
-	-v /data:/data \
-	-v /usb:/usb \
+	-v /home/mjahansh:/home/mjahansh \
+    -v /da0_data/:/da0_data/ \
+    -v /da1_data/:/da1_data/ \
+    -v /da2_data/:/da2_data/ \
+    -v /da3_data/:/da3_data/ \
+    -v /da4_data/:/da4_data/ \
+    -v /data/:/da6_data/ \
+    -v /da7_data/:/da7_data/ \
+	-w /home/mjahansh \
 	--name "$name" \
 	"$image" \
 	/bin/bash
